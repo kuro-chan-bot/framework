11765c62b756737b574fdeefa67ea0bf6bd85056
hota1024 <hotalog@hota1024.com>
2019-12-16 09:16:38 +0900

    Add HasBotcontext/index.ts

---
+++ src/abstracts/HasBotContext/index.ts
@@ -0,0 +1,34 @@
+import { HasBotContextInterface, BotContextInterface } from '../../interfaces'
+
+/**
+ * Has bot context.
+ */
+export class HasBotContext implements HasBotContextInterface {
+  /**
+   * Context.
+   */
+  context!: BotContextInterface
+
+  /**
+   * Set context.
+   *
+   * @param context
+   */
+  setContext(context: BotContextInterface) {
+    this.context = context
+  }
+
+  /**
+   * Bot.
+   */
+  get bot() {
+    return this.context.bot
+  }
+
+  /**
+   * Client.
+   */
+  get client() {
+    return this.client
+  }
+}
